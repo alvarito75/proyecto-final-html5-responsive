0003b37e44a87e925cc0608ccef661a20f3561df
Alvarito75 <alvarito75@outlook.com>
2020-03-10 14:28:14 -0400

    Responsive html5.

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,5 @@
 var dt = new Date();
 document.getElementById("datetime").innerHTML = dt.toLocaleString();
-console.log('hiiii');
 function MenuDesplegable() {
 	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
